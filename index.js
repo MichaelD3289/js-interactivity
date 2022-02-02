d8c5f74bb518bcb6d16503f52e73ab916d1be58a
Michael Drummond <michael.drummond328@gmail.com>
2022-02-02 14:09:11 -0700

    core lab completed

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-const addMovie = (event) => {
+const message = document.querySelector('#message');
+
+const addMovie = event => {
   event.preventDefault();
   const inputField = document.querySelector('input');
   let movie = document.createElement('li');
   let movieTitle = document.createElement('span');
   movieTitle.textContent = inputField.value
+
+  movieTitle.addEventListener('click', crossOffMovie);
+
   console.log(movieTitle);
   movie.appendChild(movieTitle);
+
+  const deleteBtn = document.createElement('button');
+  deleteBtn.textContent = 'X'
+  deleteBtn.addEventListener('click', deleteMovie);
+  movie.appendChild(deleteBtn);
+
   document.querySelector('ul').appendChild(movie);
   inputField.value = "";
 }
 
 document.querySelector('form').addEventListener('submit', addMovie);
+
+const deleteMovie = event => {
+  event.target.parentNode.remove();
+  message.textContent = 'Movie deleted!'
+}
+
+const crossOffMovie = event => {
+  event.target.classList.toggle('checked')
+  (event.target.classList.contains('checked') ? message.textContent = 'Movie Watched' : message.textContent = 'Movie added back!')
+}
